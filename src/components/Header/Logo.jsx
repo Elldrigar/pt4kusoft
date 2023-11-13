94e8b605700fa9b4338913de3e1705486739e7a1
Artur Gawron <artur@gawron.me>
2023-11-13 23:52:46 +0100

    Add Logo

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,8 +1,14 @@
+import logo from '@/../public/images/logo.png'
+import Image from 'next/image'
+import Link from "next/link";
+
 const Logo = () => {
     return(
-        <h1>
-            LOGO
-        </h1>
+        <Link href='/'>
+            <div className='w-28'>
+                <Image src={logo} alt='Ptakusoft' className='w-full h-auto'/>
+            </div>
+        </Link>
     )
 }
 
